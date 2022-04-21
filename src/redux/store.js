a007d64db3d5f296115ef9293afd2f7f1ba29788
zhika <zsultanova03@gmail.com>
2022-04-22 00:08:52 +0200

    cart added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
-import {createStore} from "redux"
-import reducers from "../container/reducers/index"
+import { createStore } from "redux";
 
+import reducers from "./reducers/index";
 
 const store = createStore(
-    reducers,{},
-    {},
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    
-    )
+  reducers,
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-
-export default store
+export default store;
